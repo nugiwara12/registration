8f47ef8f3c9ab0226fb481b2a7b3c538ef999650
Nugiwara_011200 <global-config@example.com>
2024-06-17 20:31:36 +0800

    add route for the SEO or search engine of the system

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\RegistrationController;
     
 Route::get('/', function () {
-    return view('home');
+    return view('auth.register');
 });
 
-Route::get('register', [RegisterController::class, 'showRegistrationForm'])->name('register');
-Route::post('register', [RegisterController::class, 'register']);
-
+Route::get('/register', [RegistrationController::class, 'showRegistrationForm'])->name('register');
+Route::post('/register', [RegistrationController::class, 'register']);
+Route::get('/registration/success/{name}', [RegistrationController::class, 'registrationSuccess'])->name('registration.success');
