78697324a94785a27bbb72f1a25742ed0d57bbb8
Nugiwara_011200 <global-config@example.com>
2024-06-17 20:29:01 +0800

    add the another locking funtion or name of the user to view all the data and fillable

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,6 +18,7 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'username',
         'email',
         'telephone',
         'add1',
@@ -25,7 +26,6 @@ class User extends Authenticatable
         'city',
         'state',
         'zip',
-        'username',
         'password',
     ];
 
@@ -48,4 +48,3 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 }
-
